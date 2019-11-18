a718217bad3bc005ff08ae2e0700f31d6ee27645
leahyseol <53325935+leahyseol@users.noreply.github.com>
2019-11-18 15:39:37 +0900

    Initial commit

---
+++ calculate1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def sum1(num1, num2):
+    return num1+num2
+
+def sub1(num1,num2):
+    return num1-num2
+
+def mul1(num1,num2):
+    return num1*num2
+
+def div1(num1,num2):
+    return num1/num2
+
---
+++ fah_converter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def convert_c_to_f(cv):
+    return cv*0.9/5+32
+
